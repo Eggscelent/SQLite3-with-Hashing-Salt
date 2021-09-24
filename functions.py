cc3140722c0a344b7751ab5ca1b3d76c1806d447
Jack <jackryder1@live.com.au>
2021-09-24 18:43:17 +1000

    changed function

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import sqlite3, uuid, bcrypt
+import sqlite3, uuid, bcrypt, sys
 from validate_email import validate_email
 from termcolor import colored
 from colorama import *
@@ -6,21 +6,13 @@ from getpass import getpass
 
 init()
 
-def validemail(useremail):
-    is_valid = validate_email(email_address=useremail, \
-        check_regex=True, check_mx=True, \
-        from_address='my@from.addr.ess', helo_host='my.host.name', \
-        smtp_timeout=10, dns_timeout=10, use_blacklist=True)
-
 def createaccount():
 
     print(colored('Create User Profile:', 'yellow'))
     useremail = input('Email: ')
-    #password = input('password: ').encode("utf-8")
     password = getpass("Password: ").encode("utf-8")
-    print(password)
 
-    validemail(useremail)
+    is_valid = validate_email(useremail, verify=True)
 
     userhashed = bcrypt.hashpw(password, bcrypt.gensalt(rounds=17))
     userid = uuid.uuid4().hex[:4]
@@ -30,22 +22,20 @@ def createaccount():
     conn = sqlite3.connect('profile.db')
     c = conn.cursor()
 
-    #create a table
-    #c.execute("""CREATE TABLE login (
-       # userid integer,
-      #  useremail text,
-     #   userhashed text
-    #)""")
+    c.execute("SELECT * from login WHERE useremail= ?", (useremail,))
+    if c.fetchone() is not None:
+        print(colored("email already exists...", "yellow"))
+    else:
+        print(colored("creating account...", "green"))
+        c.execute("insert into login (userid, useremail, userhashed) values (?, ?, ?)", (userid, useremail, userhashed))
+        conn.commit()
 
-    c.execute("insert into login (userid, useremail, userhashed) values (?, ?, ?)", (userid, useremail, userhashed))
-    conn.commit()
+        c.execute("SELECT * FROM login WHERE useremail=useremail")
 
-    c.execute("SELECT * FROM login WHERE useremail='jackryder1@live.com.au'")
+        print(c.fetchall())
 
-    print(c.fetchall())
-
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
 def login():
     conn = sqlite3.connect('profile.db')
@@ -68,7 +58,15 @@ def login():
             print(colored("Successful Login!", "green"))
         else:
             print(colored("Password incorrect.", "red"))
-            login()
     else:
         print(colored("Email does not exist", "red"))
-        login()
+
+def createtable():
+    conn = sqlite3.connect('profile.db')
+    c = conn.cursor()
+
+    c.execute("""CREATE TABLE login (
+            userid integer,
+            useremail text,
+            userhashed text
+        )""")
--- index.py
+++ index.py
@@ -5,27 +5,26 @@ from functions import createaccount, login
 
 init()
 
-while True:
-    print(colored("\n----- DB Login Test -----", "yellow"))
+def index():
+    while True:
+        print(colored("\n----- DB Login Test -----", "yellow"))
 
-    menuvalue = input("""
-    a: Create Account
-    b: Login
-    c: Exit
+        menuvalue = input("""
+        a: Create Account
+        b: Login
+        c: Exit
+    
+        Please enter your choice [A-C]: """)
 
-    Please enter your choice [A-C]: """)
+        if menuvalue == "A" or menuvalue == "a":
+            createaccount()
+        if menuvalue == "B" or menuvalue == "b":
+            login()
+        if menuvalue == "C" or menuvalue == "c":
+            print(colored("\nTerminating session...", "red"))
+            sys.exit()
+        #else:
+         #   print("You must only enter a letter from the menu")
+         #   print("Please try again")
 
-    if menuvalue == "A" or menuvalue == "a":
-        createaccount()
-
-    if menuvalue == "B" or menuvalue == "b":
-        login()
-
-    if menuvalue == "C" or menuvalue == "c":
-        print(colored("\nTerminating session...", "red"))
-
-        sys.exit()
-
-    else:
-        print("You must only enter a letter from the menu")
-        print("Please try again")
+index()
